b55241be2ef4cada2b4ff82ab655c06a003e80e5
jauhhari <ervanadiwijaya@gmail.com>
2021-07-01 08:46:11 +0700

    ok

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function cari() {
+    var kal = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.";
+
+    console.log(kal.match("are"));
+    console.log(kal.match("words"));
+}
+
+cari();
